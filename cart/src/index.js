43ec92cbc9d9e135ac9979cdd42154b0c7e60e05
Mohammadhassan <bagheri.mh69@gmail.com>
2025-07-08 18:55:56 +0200

    chores: Resolve issue for using shared library in products and cart project

--- cart/src/index.js
+++ cart/src/index.js
@@ -1,4 +1,1 @@
-import faker from "faker";
-
-const cartText = `<div>You have ${faker.random.number()} items in your card</div>`;
-document.querySelector("#dev-cart").innerHTML = cartText;
+import ('./bootstrap');
