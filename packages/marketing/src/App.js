1164ab14a6e49500f77532cb973844f35ffcf206
Mohammadhassan <bagheri.mh69@gmail.com>
2025-07-12 22:18:54 +0200

    chores: Add callback function to bootstrap file in marketing file

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,23 +1,26 @@
-import React from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { StylesProvider,createGenerateClassName } from '@material-ui/core/styles';
-import Landing from './components/Landing';
-import Pricing from './components/Pricing';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+import React from "react";
+import { Route, Router, Switch } from "react-router-dom";
+import Landing from "./components/Landing";
+import Pricing from "./components/Pricing";
 
 const generateClassName = createGenerateClassName({
-  productionPrefix: 'ma'
-})
+  productionPrefix: "ma",
+});
 
-export default () => {
+export default ({ history }) => {
   return (
     <div>
       <StylesProvider generateClassName={generateClassName}>
-        <BrowserRouter>
+        <Router history={history}>
           <Switch>
             <Route exact path="/pricing" component={Pricing} />
             <Route path="/" component={Landing} />
           </Switch>
-        </BrowserRouter>
+        </Router>
       </StylesProvider>
     </div>
   );
